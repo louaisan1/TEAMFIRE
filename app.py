45b40658731d6fbb8fca33d761e4c090ff986e5e
louaisan1 <140652073+louaisan1@users.noreply.github.com>
2024-07-19 22:37:14 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def firetime():
+    return render_template("firetime.html")
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
